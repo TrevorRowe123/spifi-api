2fb4d34947e72bb6ec55f2fdcc2570b5dae52e18
Trevor Rowe <tr968114@ohio.edu>
2018-04-23 21:51:01 +0000

    Fixed reversed Lat/Lng source functions

--- spifiapi/serializers.py
+++ spifiapi/serializers.py
@@ -9,8 +9,8 @@ class SensorSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class TargetSerializer(serializers.HyperlinkedModelSerializer):
-    targetLng = serializers.ReadOnlyField(source='getTargetLat', read_only = True)
-    targetLat = serializers.ReadOnlyField(source='getTargetLng', read_only = True)
+    targetLng = serializers.ReadOnlyField(source='getTargetLng', read_only = True)
+    targetLat = serializers.ReadOnlyField(source='getTargetLat', read_only = True)
     class Meta:
         model = Target
         fields = ('sensor', 'time', 'addr', 'targetLat', 'targetLng')
